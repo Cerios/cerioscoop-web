6f096d91e3dd681c67e9bf57246e4c2116776ceb
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-10-08 22:03:21 +0200

    cosmetic + removed obsolete WebContent directory

--- src/test/java/nl/cerios/cerioscoop/util/DateUtilsTest.java
+++ src/test/java/nl/cerios/cerioscoop/util/DateUtilsTest.java
@@ -10,8 +10,6 @@ import org.junit.Test;
 
 public class DateUtilsTest {
 
-	private static DateUtils dateUtils = new DateUtils();
-
 	@Test
 	public void testGetDaysBetween() throws ParseException {
 		SimpleDateFormat myFormat = new SimpleDateFormat("dd-MM-yyyy");
@@ -21,31 +19,31 @@ public class DateUtilsTest {
 		Date date1 = myFormat.parse(inputString1);
 		Date date2 = myFormat.parse(inputString2);
 
-		long daysBetween = dateUtils.getDaysBetween(date1, date2);
+		long daysBetween = DateUtils.getDaysBetween(date1, date2);
 		Assert.assertEquals(4, daysBetween);
-		daysBetween = dateUtils.getDaysBetween(date2, date1);
+		daysBetween = DateUtils.getDaysBetween(date2, date1);
 		Assert.assertEquals(4, daysBetween);
-		daysBetween = dateUtils.getDaysBetween(date1, date1);
+		daysBetween = DateUtils.getDaysBetween(date1, date1);
 		Assert.assertEquals(0, daysBetween);
 	}
 
 	@Test
 	public void testCalculateTime() {
-		String calculateTime = dateUtils.calculateTime(1500000);
+		String calculateTime = DateUtils.calculateTime(1500000);
 		Assert.assertEquals(0 + "j " + 2 + "w " + 3 + "d " + 8 + "h " + 40 + "m " + 0 + "s ", calculateTime);
-		calculateTime = dateUtils.calculateTime(100000000); // 0,915343915343915
+		calculateTime = DateUtils.calculateTime(100000000); // 0,915343915343915
 		Assert.assertEquals(3 + "j " + 8 + "w " + 2 + "d " + 9 + "h " + 46 + "m " + 40 + "s ", calculateTime);
-		calculateTime = dateUtils.calculateTime(167245806);
+		calculateTime = DateUtils.calculateTime(167245806);
 		Assert.assertEquals(5 + "j " + 15 + "w " + 3 + "d " + 17 + "h " + 10 + "m " + 6 + "s ", calculateTime);
-		calculateTime = dateUtils.calculateTime(2000000);
+		calculateTime = DateUtils.calculateTime(2000000);
 		Assert.assertEquals(0 + "j " + 3 + "w " + 2 + "d " + 3 + "h " + 33 + "m " + 20 + "s ", calculateTime);
-		calculateTime = dateUtils.calculateTime(5000000);
+		calculateTime = DateUtils.calculateTime(5000000);
 		Assert.assertEquals(0 + "j " + 8 + "w " + 1 + "d " + 20 + "h " + 53 + "m " + 20 + "s ", calculateTime);
-		calculateTime = dateUtils.calculateTime(22);
+		calculateTime = DateUtils.calculateTime(22);
 		Assert.assertEquals(0 + "j " + 0 + "w " + 0 + "d " + 0 + "h " + 0 + "m " + 22 + "s ", calculateTime);
-		calculateTime = dateUtils.calculateTime(50000000);
+		calculateTime = DateUtils.calculateTime(50000000);
 		Assert.assertEquals(1 + "j " + 30 + "w " + 4 + "d " + 16 + "h " + 53 + "m " + 20 + "s ", calculateTime);
-		calculateTime = dateUtils.calculateTime(90000000);
+		calculateTime = DateUtils.calculateTime(90000000);
 		Assert.assertEquals(2 + "j " + 44 + "w " + 5 + "d " + 16 + "h " + 0 + "m " + 0 + "s ", calculateTime);
 	}
 
@@ -54,7 +52,7 @@ public class DateUtilsTest {
 		final String refDate = "17-03-1997";
 		final String refTime = "23:14:51";
 
-		Date dateTime = dateUtils.toDateTime(new SimpleDateFormat("dd-MM-yyyy").parse(refDate), Time.valueOf(refTime));
+		Date dateTime = DateUtils.toDateTime(new SimpleDateFormat("dd-MM-yyyy").parse(refDate), Time.valueOf(refTime));
 
 		Assert.assertEquals(refDate + " " + refTime, new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(dateTime));
 	}
